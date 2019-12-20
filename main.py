71abb3061fb8cd3077a8f41c271cc60e05fb146c
xgreenspace <amariwest19@tamu.edu>
2019-12-20 00:22:28 -0600

    foundation

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Main:
+    def __init__(self):
+
+    def ReadTextFile(filename):
+
+        with open(filename, 'r') as file:
+            

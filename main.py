fb788da84e37e55bdb0a85aea8b588be649c4cf7
xgreenspace <amariawest@gmail.com>
2019-12-20 22:41:53 -0600

    further progress

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime as date
 
 class Main:
-    def __init__(self):
 
-    def ReadTextFile(filename):
-
+    def readtextfile(filename):
+        """
+        Reads the chat file and breaks each message into three components:
+        Time, User, Message
+        """
         with open(filename, 'r') as file:
-            
+
+            notdone = True
+
+            while notdone:
+                data = file.readline()
+                # print(data)
+
+                if data == '':
+                    notdone = False
+                    break
+
+                if data[0] == '[':
+                    k = data.index(']')
+
+                    time = data[1: k]
+                    list_time = time.split(', ')
+                    print(list_time)
+
+
+
+
+
+
+Main.readtextfile('_chat.txt')

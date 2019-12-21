ac123c0fd64e39b15b854a1bc3a8e11223ed0dde
xgreenspace <amariawest@gmail.com>
2019-12-21 15:31:43 -0600

    Change File names

---
+++ readme.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as date
+
+
+
+def readtextfile(filename):
+    """
+    Reads the chat file and breaks each message into three components:
+    Time, User, Message
+    """
+    with open(filename, 'r') as file:
+
+        notdone = True
+
+        while notdone:
+            data = file.readline()
+            # print(data)
+
+            if data == '':
+                notdone = False
+                break
+            # Breaks apart each line
+            if data[0] == '[':
+                k = data.index(']')
+
+                time = data[1: k]
+                list_time = time.split(', ')
+                # print(list_time)
+
+            if ': ' in data:
+                j = data.index(': ')
+                name = data[k + 2: j].strip()
+
+            elif 'encryption' in data:
+                name = 'Chat Encrypted!'
+                message = 'Chat Encrypted!'
+
+            else:
+                name = 'Chat Created!'
+                message = 'Chat Created!'
+
+            print(name)
+
+
+
+
+
+
+
+readtextfile('_chat.txt')

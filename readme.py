5cd5ffccfa379fe2f596c60944cb79ae2fcf8edc
xgreenspace <amariawest@gmail.com>
2019-12-22 19:40:47 -0600

    Work on handling multi-lined messages

--- readme.py
+++ readme.py
@@ -26,6 +26,7 @@ def read_format_file_no_re(filename):
             if data == '':
                 notdone = False
                 break
+
             # Breaks apart each line
             if data[0] == '[':
                 k = data.index(']')
@@ -42,6 +43,7 @@ def read_format_file_no_re(filename):
                     j = data.index(': ')
                     name = data[k + 2: j].strip()
                     message = data[j + 1:].strip()
+
                 # Identifies group creation line
                 elif 'created group' in data and not ': ' in data:
                     name = 'Chat Created!'
@@ -51,8 +53,22 @@ def read_format_file_no_re(filename):
                 list_name.append(name)
                 list_message.append(message)
 
-        print(len(master_list_time))
-        print(len(list_name))
-        print(len(list_message))
+            else:
+                last_line = ''
+
+                while not data[0] == '[':
+                    data = file.readline()
+                    message = data[:].strip()
+                    message = last_line + '\n' + message
+                    last_line = message
+                    
+                    # FIXME: Still errors with multi-lined messages
+
+            print(message)
+            print(time)
+
+        # print((master_list_time))
+        # print((list_name))
+        # print((list_message))
 
 read_format_file_no_re('_chat.txt')
